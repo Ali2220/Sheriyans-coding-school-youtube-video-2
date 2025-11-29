fbaa91908f0fd67afe30ee8d2510e3ebbc8b310f
Ali2220 <alisarwar0277@gmail.com>
2025-11-29 21:27:29 +0500

    events and event handling started

---
+++ eventListners/script.js
@@ -0,0 +1,44 @@
+// browser mein page par koi bhi harkat karo, event raise ho jae ga.
+
+// kuch screen par ho aur apko reaction dena ho to aus wqt apko event handle karna ana chahiye.
+
+// event matlb kuch action hua
+
+// event listner ka matlb hai apne koi action ka reaction dia.
+
+/* click karne par h1 ka color red ho jae  */
+// let h1 = document.querySelector('h1')
+// h1.addEventListener('click', function(){
+//     h1.style.color = 'red'
+// })
+
+/* double click karne par h1 ka color yellow ho jae. */
+// let h1 = document.querySelector('h1')
+// h1.addEventListener("dblclick", function(){
+//     h1.style.color = 'yellow'
+    
+// })
+
+
+/* event ko hta rhe hain. */
+// let h1 = document.querySelector('h1')
+// function dblClick(){
+//     h1.style.color = "yellow"
+// }
+// h1.addEventListener("dblclick", dblClick)
+// h1.removeEventListener("dblclick", dblClick)
+
+
+/* "input" event tab fire hota hai jab user type kare, delete kare, copy-paste kare → basically value change ho */
+// let input = document.querySelector('input')
+// input.addEventListener("input", function(e){
+//     console.log(e.target.value);
+// })
+
+/* Sirf type hone wale characters ko log karo, deletion(backspace click krna) ko ignore karo. */
+// let input = document.querySelector('input')
+// input.addEventListener("input", function(details){
+//     if(details.data !== null){
+//         console.log(details.data);
+//     }
+// })
